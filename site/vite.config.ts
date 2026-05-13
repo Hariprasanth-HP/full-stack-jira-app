a53ac13f43680bfdb0d275726b02b297aa9b1a00
Hari K <hari16205@gmail.com>
2026-05-13 10:29:48 +0530

    stop tracking env files

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,23 +1,26 @@
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
-// https://vite.dev/config/
-export default defineConfig({
-	base: process.env.VITE_BASE_PATH || "/",
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:4000", // Your backend URL
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ""), // Remove /api prefix
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		base: env.VITE_BASE_PATH || "/",
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
 			},
 		},
-	},
+		server: {
+			proxy: {
+				"/api": {
+					target: "http://localhost:4000", // Your backend URL
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ""), // Remove /api prefix
+				},
+			},
+		},
+	};
 });
