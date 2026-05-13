4103efb56e2356114987aefd6462ee101ccf3fe9
Hari K <hari16205@gmail.com>
2026-05-13 10:47:11 +0530

    stop tracking env files

--- site/src/app/routes/signup/signup.tsx
+++ site/src/app/routes/signup/signup.tsx
@@ -7,33 +7,33 @@ import { supabase } from "@/lib/supabase";
 import type { SignupPayload } from "@/types/auth";
 
 export default function SignupPage() {
-	const dispatch = useAppDispatch();
-	const navigate = useNavigate();
-	async function signup(userData: SignupPayload) {
-		await dispatch(signupUser(userData));
-		await navigate("/");
-	}
-	const handleGoogleSignup = async () => {
-		const { error } = await supabase.auth.signInWithOAuth({
-			provider: "google",
-			options: {
-				redirectTo: `${window.location.origin}/callback`,
-				queryParams: {
-					prompt: "select_account",
-				},
-			},
-		});
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  async function signup(userData: SignupPayload) {
+    await dispatch(signupUser(userData));
+    await navigate("/");
+  }
+  const handleGoogleSignup = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/${import.meta.env.VITE_BASE_PATH}/callback`,
+        queryParams: {
+          prompt: "select_account",
+        },
+      },
+    });
 
-		if (error) {
-			toast.error("Google signup failed");
-			return;
-		}
-	};
-	return (
-		<div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-			<div className="w-full max-w-sm md:max-w-4xl">
-				<SignupForm onSubmit={signup} handleGoogleSignup={handleGoogleSignup} />
-			</div>
-		</div>
-	);
+    if (error) {
+      toast.error("Google signup failed");
+      return;
+    }
+  };
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <SignupForm onSubmit={signup} handleGoogleSignup={handleGoogleSignup} />
+      </div>
+    </div>
+  );
 }
